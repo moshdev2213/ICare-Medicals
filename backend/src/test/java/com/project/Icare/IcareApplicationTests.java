f5a242b9cce65b42e27222319c9c562bdf3448de
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-08-07 09:01:16 +0530

    addedBackend

---
+++ backend/src/test/java/com/project/Icare/IcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Icare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
