3ea04c38e497154343b418a0c6944081c587daa1
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-08-08 15:07:24 +0530

    DoctorDetailsDone

---
+++ frontend/IcareMobile/app/src/main/java/com/example/myapplication/DoctorDetails.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DoctorDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctor_details)
+    }
+}
