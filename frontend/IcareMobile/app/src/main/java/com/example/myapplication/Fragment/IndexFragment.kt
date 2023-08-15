08f708ca3e7006012d040a8b9ac5c088da0ab7cb
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-08-15 23:30:21 +0530

    imageUploadToBrDonr

--- frontend/IcareMobile/app/src/main/java/com/example/myapplication/Fragment/IndexFragment.kt
+++ frontend/IcareMobile/app/src/main/java/com/example/myapplication/Fragment/IndexFragment.kt
@@ -1,60 +1,42 @@
 package com.example.myapplication.Fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import com.example.myapplication.EyeDiseases
+import com.example.myapplication.EyeScan
+import com.example.myapplication.Home
 import com.example.myapplication.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [IndexFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class IndexFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var cvScanEye:CardView
+    private lateinit var cvChannel:CardView
+    private lateinit var cvDisease:CardView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_index, container, false)
+        val view = inflater.inflate(R.layout.fragment_index, container, false)
+
+        val home = requireActivity() as Home
+        cvScanEye =view.findViewById(R.id.cvScanEye)
+        cvDisease =view.findViewById(R.id.cvDisease)
+        cvChannel =view.findViewById(R.id.cvChannel)
+
+        cvScanEye.setOnClickListener {
+            startActivity(Intent(requireActivity(),EyeScan::class.java))
+        }
+        cvDisease.setOnClickListener {
+            startActivity(Intent(requireActivity(),EyeDiseases::class.java))
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment IndexFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            IndexFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
