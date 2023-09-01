3f16334ba8bdc535e927f05e8e2701193f545060
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-09-01 20:49:44 +0530

    addedDoctorGetAll

--- backend/src/main/java/com/project/Icare/Repo/DoctorRepo.java
+++ backend/src/main/java/com/project/Icare/Repo/DoctorRepo.java
@@ -3,7 +3,11 @@ package com.project.Icare.Repo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.project.Icare.Collection.Doctor;
+import java.util.List;
+import java.util.Optional;
+
 
 public interface DoctorRepo extends MongoRepository<Doctor, String> {
+	Optional<List<Doctor>> findByStatus(int status);
 
 }
